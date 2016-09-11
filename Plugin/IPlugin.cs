b5c79a60f037a2814b6dc43cdb81f65be079bd23
sontx <xuanson33bk@gmail.com>
2016-09-11 14:02:31 +0700

    + define plugin interface

---
+++ Plugin/IPlugin.cs
@@ -0,0 +1,10 @@
+﻿namespace Plugin
+{
+    public interface IPlugin
+    {
+        void Install();
+        void Uninstall();
+        void Load(string workingDir);
+        void Unload();
+    }
+}
